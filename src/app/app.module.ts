cb8f691e7b58db0cd6aee90fb4564f87dc955fc1
fixruan <ruangoio01@gmail.com>
2022-12-28 11:53:32 -0300

    directives ngClass - ngStyle

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { ParentComponent } from './components/parent/parent.component';
+import { DirectivesComponent } from './components/directives/directives.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ParentComponent
+    ParentComponent,
+    DirectivesComponent
   ],
   imports: [
     BrowserModule,
